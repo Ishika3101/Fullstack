cb69a4e4f4131cc2fba8d1f85c1fa0854a995e49
Ishika Goyal <goyal.ishika6789@gmail.com>
2026-03-27 09:55:47 +0530

    part3 done

--- database/8.3 Travel Tracker/index.js
+++ database/8.3 Travel Tracker/index.js
@@ -33,17 +33,40 @@ app.get("/", async (req, res) => {
  //post method for adding country that are visited
 app.post("/add",async(req,res)=>{
   const input=req.body["country"]; //where user types country in the name =country
-  const result=await db.query("SELECT country_code FROM countries WHERE country_name=$1",[input]); //dynamically insert data in place of $1
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name = $1",
+      [input]
+    );
 
-  if(result.rows.length!==0){
-    const data=result.rows[0];
-    const country_code=data.country_code;
-
-    await db.query("INSERT INTO visited_countries(country_code) VALUES($1)",[country_code]);
-    res.redirect("/");
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
-
-})
+});
+  
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
